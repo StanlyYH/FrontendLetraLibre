fc24546703063e41528fd0c1247911a4067193db
darlanmejia <daelanjacob@gmail.com>
2026-08-01 16:27:56 -0600

    feat(libros): Creando tipos de Libro

---
+++ src/types/libro.types.ts
@@ -0,0 +1,14 @@
+export interface Libro {
+    id: string;
+    titulo: string;
+    autor: string;
+    isbn: string;
+    editorial: string;
+    categoria: string;
+    fechaPublicacion: string;
+    precio: number;
+    stock: number;
+    descripcion: string;
+    imageUrl: string;
+}
+
